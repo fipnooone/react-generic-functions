64411194db503adca6d7f091fda0eddeb83e2419
fipnooone <fipnone@gmail.com>
2024-01-18 11:43:50 +0700

    fix: ts fixes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import React, { ForwardedRef, ReactElement, ReactNode, RefAttributes } from 'react';
+import React, { ForwardedRef, ReactNode, RefAttributes } from 'react';
 
 export const memo: <T>(c: T) => T = React.memo;
-// eslint-disable-next-line @typescript-eslint/ban-types
-export const forwardRef = <T, P = {}>(render: (props: P, ref: ForwardedRef<T>) => ReactNode) =>
-    React.forwardRef(render) as (props: P & RefAttributes<T>) => ReactElement | null;
+// eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
+export const forwardRef = <T, P = any>(render: (props: P, ref: ForwardedRef<T>) => ReactNode) =>
+    React.forwardRef(render) as (props: P & RefAttributes<T>) => ReactNode;
